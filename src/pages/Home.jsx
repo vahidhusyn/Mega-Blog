12de8896680a49de94a789ae434d338b7e3a9a7e
Vahid <vahidhusyn@gmail.com>
2024-08-11 21:53:06 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react'
+import appwriteService from "../appwrite/config";
+import {Container, PostCard} from '../components'
+
+function Home() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        appwriteService.getPosts().then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
+  
+    if (posts.length === 0) {
+        return (
+            <div className="min-h-screen py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl  font-bold">
+                                No Posts to Display!
+                            </h1>
+                            <h2>
+                                Please login to view posts...
+                            </h2>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+    return (
+        // this div is for loggedin posts
+        <div className='w-full py-8'> 
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home
